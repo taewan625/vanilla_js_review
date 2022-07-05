856218ac3d04a26ccee10e82fc123d2960e708cc
taewan625 <taewan1997625@gmail.com>
2022-07-05 17:40:41 +0900

    #2.8
    object 안에 들어가 있는 function 만들고
    출력하는 방법
    object.functionNameObject();

--- app.js
+++ app.js
@@ -2,13 +2,39 @@
 
 // function 에서 ()는 function을 실행하는 부분
 // {} 부분이 반복 실행 됨
-function functionName() {
-  console.log("hello my name is ");
+function functionName(variableName, secondVariableName) {
+  console.log("hello my name is ", variableName, secondVariableName);
 }
 // function 실행 ()가 존재하면 됨
-functionName("a");
-functionName("b");
-functionName("c");
-functionName("d");
+functionName("firstArgument", "secondArgument");
+functionName("b", 2);
+functionName("c", 3);
+functionName("d", 4);
+// functionName("text",1); 값을 넣었을 때 function 에서 값을 나타내는 법 / variableName은 2개까지 넣을 수 있다
 
-// functionName("text"); 값을 넣었을 때 function 에서 값을 나타내는 법
+function counter(a, b) {
+  console.log(a * b);
+  console.log(a / b);
+}
+counter(1, 2);
+counter(2, 2);
+counter(4, 2);
+counter(5, 2);
+
+// ####아주 중요#### object 안에서 function 이용하는 방법
+const object = {
+  property1: "a",
+  property2: "b",
+  functionNameObject: function (variableName) {
+    console.log("c", variableName);
+  },
+};
+console.log(object.property1); // 따로 function으로 만들어지지 않은 object들은 console.log를 이용해서 가져오고
+object.functionNameObject("argument"); // object 내에 만든 function으로 만들어진 property는 다이렉트로 object.functionNameObject(); 형식으로 진행 -> 콘솔창에도 잘 나옴
+
+const practiceOne = {
+  fno: function (a, b) {
+    console.log(a + b);
+  },
+};
+practiceOne.fno(3431, 123);
