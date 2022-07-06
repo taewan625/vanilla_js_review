816b7a693fa6aa5c5dd1891cda0835b75d9288ae
taewan625 <taewan1997625@gmail.com>
2022-07-06 20:15:19 +0900

    #2.13
    1. js의 이상한 기능
    2. JS는 안에서부터 바깥으로 결과를 도출한다

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const objectReturn = {
-  functionNameObjectReturn: function (
-    variableNameOneReturn,
-    variableNameTwoRetrun
-  ) {
-    return `${variableNameOneReturn} ${variableNameTwoRetrun}`;
-    console.log(
-      "return값이 나오고 나면 function은 작동을 멈춰서 이 console.log 값은 나오지 않는다."
-    );
-  },
-};
-const needThisVariableForGetFunctionResult =
-  objectReturn.functionNameObjectReturn("argument3", "argument4");
+// conditional 조건문
 
-// 복습
-// return을 하면 function은 작동을 멈추고 결과 값을 return하고 끝내버린다
-// const needThisVariableForGetFunctionResult 값을 이용해서 HTML 밖으로 결과를 내는 과정 중 준비단계라고 생각하면 된다
+console.log(typeof parseInt(age)); // JS 이상한 기능
+
+const age = parseInt(prompt("how old are you?")); // JS는 안에서 부터 바깥으로 결과를 도출한다
+console.log(age); // JS 이상한 기능
+
+parseInt(); // string -> Inst
