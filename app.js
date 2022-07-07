d64e28c0d0eaffdb8b62c4fc4b76ee5bd91f39b2
taewan625 <taewan1997625@gmail.com>
2022-07-07 21:36:00 +0900

    #3.4
    querySelector
    addeventlistener

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// ##### 자주 사용! #####
-const title = document.getElementById("title");
-console.dir(title.innerText);
-title.innerText = "Change!!";
+// Uncaught TypeError: Cannot read properties of null (reading 'innerText') at app.js:3:19
+// 이 오류는 title이 존재하지 않으니 내부의 innerText를 호출하지 마시오 랑 같은 의미
 
-console.log(title.id);
-console.log(title.class); // getElemenById로 id를 이용해서 연결한 것이라서 class를 찾을 수 없음
-console.log(title.className); // 하지만 id가 title인 tag에서 classNAme의 정보는 가지고 올 수 있다
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
