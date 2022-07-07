83f26098bef1d43fb4974e723ac9aa13fecab5e0
taewan625 <taewan1997625@gmail.com>
2022-07-07 15:41:50 +0900

    #2.16
    if문안에 if문 ㅋㅋ

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // conditional 조건문
 
-const age = parseInt(prompt("how old are you?")); // JS는 안에서 부터 바깥으로 결과를 도출한다 // ParserInt로 type을 나타낸다
+const age = parseInt(prompt("how old are you?"));
 
 const condition = isNaN(age);
 
@@ -11,9 +11,18 @@ function ifReturn(condition) {
     return "minor can't drink";
   } else if (age >= 18 && age <= 80) {
     return "Welcome to bar";
-  } else if (age > 90) {
-    return "Think about your health";
+  } else if (age === 100) {
+    // ===, !==
+    console.log("wowee!!");
+  } // JS는 위에서부터 아래로 실행되고 return값은 받고 나서부터 function이 멈춰서 작동을 안함
+  // 그래서 100살 console.log 받으려면 age >100보다 위에 있으면 작동함
+  else if (age > 90) {
+    if (age < 100) {
+      return "Think about your health";
+    } else {
+      return "holy shit";
+    }
+    // else if 안에도 if문을 넣을 수 있다
   }
-  // else는 옵션이여서 굳이 안써도 된다
 }
 const returnResult = ifReturn(condition);
