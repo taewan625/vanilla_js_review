2e78270eecc6d3502008cc6efb530069025672af
taewan625 <taewan1997625@gmail.com>
2022-07-07 17:43:31 +0900

    #3.0
    document =  html을 object 형식으로 보여줌

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-// conditional 조건문
+console.dir(document);
+// document =  html을 object 형식으로 보여줌
 
-const age = parseInt(prompt("how old are you?"));
+document.title; // 'taewanJS'
+// JS는 HTML에 접근하고 읽을 수 있도록 설정이 되어있다.
 
-const condition = isNaN(age);
-
-function ifReturn(condition) {
-  if (condition || age < 0) {
-    return "Please write an age";
-  } else if (age < 18) {
-    return "minor can't drink";
-  } else if (age >= 18 && age <= 80) {
-    return "Welcome to bar";
-  } else if (age === 100) {
-    // ===, !==
-    console.log("wowee!!");
-  } // JS는 위에서부터 아래로 실행되고 return값은 받고 나서부터 function이 멈춰서 작동을 안함
-  // 그래서 100살 console.log 받으려면 age >100보다 위에 있으면 작동함
-  else if (age > 90) {
-    if (age < 100) {
-      return "Think about your health";
-    } else {
-      return "holy shit";
-    }
-    // else if 안에도 if문을 넣을 수 있다
-  }
-}
-const returnResult = ifReturn(condition);
+document.title = "taewanJSchangeName";
+// JS array나 object에 추가하는 것처럼 하면 수정이 됨
