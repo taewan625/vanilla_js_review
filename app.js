6b03f4e109130e00b090e2848b63f8972dcb184a
taewan625 <taewan1997625@gmail.com>
2022-07-08 10:22:22 +0900

    #3.5
    console.dir(target) 안에 on###이 event
    event mdn으로도 찾기 가능

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // Uncaught TypeError: Cannot read properties of null (reading 'innerText') at app.js:3:19
 // 이 오류는 title이 존재하지 않으니 내부의 innerText를 호출하지 마시오 랑 같은 의미
 
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
+
+console.dir(h1); // 여기서 on이 붙은 것이 eventlistener이다
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
+  h1.innerText = "The mouse is here!";
 }
-
-title.addEventListener("click", handleTitleClick);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "green";
+}
+h1.addEventListener("mouseenter", handleTitleClick);
+window.addEventListener("resize", handleWindowResize);
+// = h1.onmouseenter = handleTitleClick;
+// HTMLHeadingElement-web APIs ; JS 요소들
