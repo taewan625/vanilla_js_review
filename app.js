215633b1c9c8bc6e8f59b4b8f26c9d6902fbcc4d
taewan625 <taewan1997625@gmail.com>
2022-07-11 18:30:25 +0900

    #4.7 update
    paintGreetins argument에 아무것도 않넣고 이용하는 방법

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username); // input에 넣을때 그 순간에 보여주기 위함
+  paintGreetings();
+  // input에 넣을때 그 순간에 보여주기 위함 -> () 없애면 아무것도 안니오지만 refresh 할때 나옴
 }
 
 // loginForm.addEventListener("submit", onLoginSubmit);
 // 초기값처럼 여기 있어도 되고
 
-function paintGreetings(anythingCan) {
-  greeting.innerText = `hello, ${anythingCan}`;
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  // function 안의 const는 function 안에서만 영향을 미친다
+  greeting.innerText = `hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -29,5 +32,5 @@ if (getitemUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
   // 여기로 넣어도 상관 없음
 } else {
-  paintGreetings(getitemUsername); // 새로고침을 하고 나서도 보여주기 위함
+  paintGreetings(); // 새로고침을 하고 나서도 보여주기 위함
 }
