ac324ac76ed5d74441c0377a8708a2dc09400c4b
taewan625 <taewan1997625@gmail.com>
2022-07-11 18:28:44 +0900

    #4.7
    paintGreetings에 argument 넣어서 localstorage 1번만 확인하기

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); // browser의 새로고침을 막아주는 역할
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  // greeting.innerText = `hello, ${username}`;
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
-  paintGreetings(username);
+  paintGreetings(username); // input에 넣을때 그 순간에 보여주기 위함
 }
 
 // loginForm.addEventListener("submit", onLoginSubmit);
@@ -31,7 +29,5 @@ if (getitemUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
   // 여기로 넣어도 상관 없음
 } else {
-  // greeting.innerText = `hello, ${getitemUsername}`;
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
-  paintGreetings(getitemUsername);
+  paintGreetings(getitemUsername); // 새로고침을 하고 나서도 보여주기 위함
 }
