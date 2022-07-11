d568eb8a6b808270ed5b2b7c7824f4e3cf0f262a
taewan625 <taewan1997625@gmail.com>
2022-07-12 01:08:06 +0900

    challengeMyone3

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -1,13 +1,34 @@
-const fontColor = document.querySelector("h1");
+const playForm = document.querySelector("form");
+const maxNumber = playForm.querySelector("#max");
+const playerNum = playForm.querySelector("#playerNum");
 
-function changeThreeColor() {
-  if (window.innerWidth > 1000) {
-    document.body.style.backgroundColor = "yellow";
-  } else if (window.innerWidth < 1000 && window.innerWidth > 500) {
-    document.body.style.backgroundColor = "purple";
+const detail = document.querySelector(".detail");
+const result = document.getElementById("innerHTML");
+function Playbutton(pause) {
+  pause.preventDefault(); // 일시정지
+
+  const changeToInt = parseInt(maxNumber.value);
+  // input 값을 value를 이용해 가저옴 -> string이기 때문에 int로 변경
+
+  const multiNum = changeToInt + 1;
+  const randomNum = Math.floor(Math.random() * multiNum);
+  localStorage.setItem("random", randomNum);
+
+  playerNum.setAttribute("max", changeToInt);
+  // attribute 추가 max값 이내로 제한
+
+  const changeToIntTwo = parseInt(playerNum.value);
+  localStorage.setItem("betNum", changeToIntTwo);
+
+  const me = localStorage.getItem("betNum");
+  const machine = localStorage.getItem("random");
+  detail.innerText = `You chose: ${me}, the machine chose: ${machine}.`;
+
+  if (me === machine) {
+    result.innerHTML = "You Won!";
   } else {
-    document.body.style.backgroundColor = "blue";
+    result.innerHTML = "You lost!";
   }
-  fontColor.style.color = "white";
 }
-window.addEventListener("resize", changeThreeColor);
+
+playForm.addEventListener("submit", Playbutton);
