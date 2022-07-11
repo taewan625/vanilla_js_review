1f19e086675a66a3b286b790c35165218c113caa
taewan625 <taewan1997625@gmail.com>
2022-07-11 18:09:50 +0900

    #4.5 -4.6
    #### 아주 중요한 개념 ####
    function의 argument는 아무거나 내가 원하는거 쓰는 거임

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `hello, ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  // greeting.innerText = `hello, ${username}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+}
+
+// loginForm.addEventListener("submit", onLoginSubmit);
+// 초기값처럼 여기 있어도 되고
+
+function paintGreetings(anythingCan) {
+  greeting.innerText = `hello, ${anythingCan}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
-// js는 함수를 실행시키는 동시에 그 함수에 첫번때 인자에 object를 넣어준다
+
+const getitemUsername = localStorage.getItem(USERNAME_KEY);
+
+if (getitemUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+  // 여기로 넣어도 상관 없음
+} else {
+  // greeting.innerText = `hello, ${getitemUsername}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(getitemUsername);
+}
