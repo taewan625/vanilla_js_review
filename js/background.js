30dcb1da7bca90e12cb9cecaa9c4c4bc55dc0fbb
taewan625 <taewan1997625@gmail.com>
2022-07-12 21:28:55 +0900

    #6.1-2
    js에서 html 넣는 법
    
    1) const variable = document.createElement("tagName")
    
    2) variable.argument = `text or ${}`
    3) document.body.appendChild(variable)

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
+const randomImage = images[Math.floor(Math.random() * images.length)];
+const jsToHTMLImages = document.createElement("img");
+jsToHTMLImages.src = `img/${randomImage}`;
+// console.log(jsToHTMLImages);
+
+// 여기까지가 js에서 img tag를 만드는 것이고 이제 HTML에 넣을 것이다
+
+document.body.appendChild(jsToHTMLImages);
+// jsToHTMLImages.src = `img/${randomImage}`; 이게 존재하지 않으면 html에 그냥 <img> tag만 존재
+
+// JS -> HTML로 tag 넣는 법
+// 1) const variable = document.createElement("tagName")
+// 2) variable.argument = `text or ${}`
