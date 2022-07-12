27678cd1f1bc112a56b9c3c666f5bda722b0a72a
taewan625 <taewan1997625@gmail.com>
2022-07-12 19:26:19 +0900

    #5.0
    setInterval : 실시간 상황 체크
    argument: function, ms

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("#clock");
+
+function sayHello() {
+  console.log("hello");
+}
+
+setInterval(sayHello, 5000);
+// setInterval ; 실시간 점검이 필요할 때 사용하는 함수
--- js/greetings.js
+++ js/greetings.js
@@ -11,17 +11,14 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings();
-  // input에 넣을때 그 순간에 보여주기 위함 -> () 없애면 아무것도 안니오지만 refresh 할때 나옴
+  paintGreetings(username); // input에 넣을때 그 순간에 보여주기 위함
 }
 
 // loginForm.addEventListener("submit", onLoginSubmit);
 // 초기값처럼 여기 있어도 되고
 
-function paintGreetings() {
-  const username = localStorage.getItem(USERNAME_KEY);
-  // function 안의 const는 function 안에서만 영향을 미친다
-  greeting.innerText = `hello, ${username}`;
+function paintGreetings(anythingCan) {
+  greeting.innerText = `hello, ${anythingCan}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -32,5 +29,5 @@ if (getitemUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
   // 여기로 넣어도 상관 없음
 } else {
-  paintGreetings(); // 새로고침을 하고 나서도 보여주기 위함
+  paintGreetings(getitemUsername); // 새로고침을 하고 나서도 보여주기 위함
 }
