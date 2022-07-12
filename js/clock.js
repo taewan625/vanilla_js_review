259e2746bb32afd304953f9a9800354fd80ba923
taewan625 <taewan1997625@gmail.com>
2022-07-12 21:04:14 +0900

    #6.0

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,11 @@
-const clock = document.querySelector("#clock");
+const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date(); // date에 관한 모든 정보 함유
-  const day = date.getDay();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${day}, ${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock, 1000); // 정한 시간만다 function을 실행
