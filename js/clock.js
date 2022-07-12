6a81733d8efabb2fac329dc798ae2e367d5093de
taewan625 <taewan1997625@gmail.com>
2022-07-12 20:06:41 +0900

    #5.2-3
    // "string".padStart(2,"0") ;  "string"이 2자리가 아닐시 앞에 "0"이란 string을 넣으세요
    // padEnd도 존재
    // String(number) => string으로 변환
    
    // new date : 생산자 함수 -> 공부필요

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,15 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date(); // date에 관한 모든 정보 함유
-  clock.innerText = `${date.getDay()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const day = date.getDay();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${day}, ${hours}:${minutes}:${seconds}`;
 }
 getClock();
-setInterval(getClock, 1000);
+setInterval(getClock, 1000); // 정한 시간만다 function을 실행
+
+// "string".padStart(2,"0") ;  "string"이 2자리가 아닐시 앞에 "0"이란 string을 넣으세요
+// padEnd도 존재
+// String(number) => string으로 변환
