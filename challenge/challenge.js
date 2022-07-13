ab5ba10038ecd0a7b979e289b37aa36f6bfd62d6
taewan625 <taewan1997625@gmail.com>
2022-07-13 16:13:21 +0900

    challenge3_solution

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -1,34 +1,22 @@
-const playForm = document.querySelector("form");
-const maxNumber = playForm.querySelector("#max");
-const playerNum = playForm.querySelector("#playerNum");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+const maxNumber = document.getElementById("maxNumber");
 
-const detail = document.querySelector(".detail");
-const result = document.getElementById("innerHTML");
-function Playbutton(pause) {
-  pause.preventDefault(); // 일시정지
-
-  const changeToInt = parseInt(maxNumber.value);
-  // input 값을 value를 이용해 가저옴 -> string이기 때문에 int로 변경
-
-  const multiNum = changeToInt + 1;
-  const randomNum = Math.floor(Math.random() * multiNum);
-  localStorage.setItem("random", randomNum);
-
-  playerNum.setAttribute("max", changeToInt);
-  // attribute 추가 max값 이내로 제한
-
-  const changeToIntTwo = parseInt(playerNum.value);
-  localStorage.setItem("betNum", changeToIntTwo);
-
-  const me = localStorage.getItem("betNum");
-  const machine = localStorage.getItem("random");
-  detail.innerText = `You chose: ${me}, the machine chose: ${machine}.`;
-
-  if (me === machine) {
-    result.innerHTML = "You Won!";
-  } else {
-    result.innerHTML = "You lost!";
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "" && maxNumber === "") {
+    return;
   }
+  const max = maxNumber.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
 }
 
-playForm.addEventListener("submit", Playbutton);
+guessForm.addEventListener("submit", handleGuessSubmit);
