e692a6ea385406d67102b189496d8754b17eb89a
taewan625 <taewan1997625@gmail.com>
2022-07-16 21:04:06 +0900

    #7.3-4
    Json stringify & parse
    list data 저장 하는 방법
    .forEach

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
+const TODOS_KEY = "saveToDosKey";
 
+const saveToDosList = [];
+
+// 시작 function
 function handleToDoSubmit(event) {
   event.preventDefault();
   const saveInputTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(saveInputTodo);
+  saveToDosList.push(saveInputTodo); // saveInputTodo를 saveToDosList에 push할 것
+  paintToDo(saveInputTodo); // paintToDo function으로 이동
+  saveToDos(); // local storage 저장
 }
+toDoForm.addEventListener("submit", handleToDoSubmit);
 
-function paintToDo(argument) {
+// list 만드는 function
+function paintToDo(argumentNewTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span"); // li > span 하기 위해서
-  span.innerText = argument;
+  span.innerText = argumentNewTodo;
   const button = document.createElement("button");
   button.innerText = "👌";
   button.addEventListener("click", deleteToDo);
@@ -23,7 +30,35 @@ function paintToDo(argument) {
   toDoList.appendChild(li); // ul > li 넣기
 }
 
+// list data 저장 function
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(saveToDosList));
+  //  Json 함수를 사용하는 이유
+  // 원래 local storage는 array 형태를 저장하지 못하고 저장시 string만 나열된 상태로 나옴
+  // saveToDosKey    1,2,3
+
+  // 하지만 array를 이용해야하므로 array 형태의 string으로 우선 만들어줌 <- JSON.stringify()
+  // saveToDosKey	["1","2","3"]  -> 이렇게 나오는데 [] 까지 포함한 string 이다.
+  // console창 :localStorage.getItem("saveToDosKey") => '["1","2","3"]'
+
+  // 이것의 string을 없애서 array 형태로 나타내게 하는 것이 <- JSON.parse()
+  // console창 :JSON.parse(localStorage.getItem("saveToDosKey")) => (3) ['1', '2', '3']
+}
+
+// delete 하는 function
 function deleteToDo(argumentEvent) {
   const findDeletToDoLi = argumentEvent.target.parentElement;
   findDeletToDoLi.remove();
+}
+
+// array 추출
+const saveToDosArray = localStorage.getItem(TODOS_KEY);
+
+if (saveToDosArray) {
+  // todolist 값이 아무것도 없을 때 방지 하기 위함
+  // if(saveToDOsArray) 자체만으로도 만약 saveToDOsArray가 존재할때 밑에껄 실행해줘 의미
+  // if(saveToDOsArray !== null)도 같은 말
+  const parsedToDOs = JSON.parse(saveToDosArray);
+  parsedToDOs.forEach((eachItem) => console.log("todolist", eachItem));
+  // .forEach ; array안의 list를 모두 순찰
 }
